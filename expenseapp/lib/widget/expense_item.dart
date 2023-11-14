eb4d4eae77539ecdc8ed3222d0081f81209f84da
aydnlramazan <106530954+aydnlramazan@users.noreply.github.com>
2023-11-14 13:56:36 +0300

    spacer, date format, card class,

---
+++ expenseapp/lib/widget/expense_item.dart
@@ -0,0 +1,39 @@
+import 'package:expenseapp/models/expense.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class ExpenseItem extends StatelessWidget {
+  //unnamed argument
+  const ExpenseItem(this.expense, {Key? key}) : super(key: key);
+  final Expense expense;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Text(expense.name),
+            Row(
+              //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                    "${expense.price.toStringAsFixed(2)} ₺"), //string interpolation
+                const Spacer(), //kullanıldığında alabildiği tüm alanı kaplar!
+                //
+                Text(DateFormat.yMd().format(expense.date)), //TODO: Format Date
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  } //y=> year
+  //M=> mount
+  //M=>minute
+  //H=>24 lük saat dilimde saat
+  //h=>12lik saat dilimde saat
+  //d=>day
+  //s=>second
+}
